1725108cc8eb25e702a2fb3a7d4199bfb6bbb71c
Sergey <serhiopehota1991@gmail.com>
2021-03-04 19:31:25 +0300

    Add ViewController lifecycle handling

--- Currency/Currency/ViewController.swift
+++ Currency/Currency/ViewController.swift
@@ -3,7 +3,14 @@
 //  Currency
 //
 //  Created by Sergey on 28.02.21.
-//
+// запустить cmd+r или треугольник
+//  жизненный цикл Сначала viewDidLoad viewWillAppear viewWillLayoutSubviews viewDidLayoutSubviews viewDidAppear
+// класс - в модели мы декларируем свойства и методы класса
+// класс не исполняемая среда как песочница, код не исполняется сверху вниз
+
+
+
+
 
 import UIKit
 
@@ -11,9 +18,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("viewDidLoad")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewWillAppear")
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("viewDidAppear")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("viewWillLayoutSubviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("viewDidLayoutSubviews")
+    }
 }
 
